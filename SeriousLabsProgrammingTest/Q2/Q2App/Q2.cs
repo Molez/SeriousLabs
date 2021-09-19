7253f105962deef04e782a0dcaf62db28fc6a00f
James Hodgson <hodgson.james.r@gmail.com>
2021-09-19 02:23:11 -0600

    Adding in some solutions for Q2

--- SeriousLabsProgrammingTest/Q2/Q2App/Q2.cs
+++ SeriousLabsProgrammingTest/Q2/Q2App/Q2.cs
@@ -1,7 +1,9 @@
-﻿namespace Q2App
+﻿using System;
+
+namespace Q2App
 {
     //Feels like a good idea to make this struct immutable
-    public readonly struct Vec3
+    public readonly struct Vec3 : IEquatable<Vec3>
     {
         public double X { get;  }
         public double Y { get; }
@@ -12,6 +14,12 @@
             Y = y;
             Z = z;
         }
+
+        public override bool Equals(object obj) => obj is Vec3 other && this.Equals(other);
+
+        public bool Equals(Vec3 other) => X == other.X && Y == other.Y && Z == other.Z;
+
+        public override int GetHashCode() => (X, Y, Z).GetHashCode();
     }
 
     public static class Q2
@@ -34,43 +42,138 @@
          * 
          * I think there is a way to project 3D points into a 2D plane (probably how they do normal mapping) solve the problem and then return back to the 3D space but I am not 
          * 100% sure how to do this. 
+         * 
+         * I added implementation of various vector operations as a primer for figuring this all out. Implemeted things such as add/subtract, dot product,
+         * scalar multiplication, magnitude, etc.
+         * 
+         * I then found a few different implementations of this problem and implemented and tested them (as best I could using simple planes with one dimention as 0 to simplify the calculations).
+         * I used these different implementations to wrap my head around the problem. Some worked and some didnt but I left them all in as examples of my work. I feel fairly comfident
+         * I implemented them as described but it is possible there is small issues... it is currently 2am...
          **/
+
+
+        // Implementation of a solution found here
+        // https://forum.unity.com/threads/how-do-i-find-the-closest-point-on-a-line.340058/
+        // This one is giving me wildly different answers in many cases so I abandoned it without putting too much more time into it.
         public static Vec3 GetNearestPointOnSegment(Vec3 point, Vec3 start, Vec3 end)
         {
-            //N is considered to be the point on the line that is perpendicular to point. λN is then the distance from start to N where a value between 0 and 1 means its between 
-            //end
+            var segmentVector = Subtract(end, start);
+            var segmentMagnitude = Magnitude(segmentVector);
+            var normalizedSegmentVector = Normalize(segmentVector);
 
-            var lambdaN = GetLambdaN(point, start, end);
+            var pointVector = Subtract(point, start);
+            var pointAndSegmentDotProduct = Dot(pointVector, normalizedSegmentVector);
+            var clampedDotProduct = Clamp(pointAndSegmentDotProduct, 0, segmentMagnitude);
 
-            if (lambdaN <= 0 )
+            return ScalarMultiplication(Add(start, normalizedSegmentVector), clampedDotProduct);
+        }
+
+        //Implementation of a solution found here
+        //https://zalo.github.io/blog/closest-point-between-segments/
+        //My testing implies this one works well. It is essentially the same as the 4th implementation
+        //it just solves the t < 0 and t > 1 solution using clamp instead and does a Lerp to find the final point
+        public static Vec3 GetNearestPointOnSegment2(Vec3 point, Vec3 start, Vec3 end)
+        {
+            var segmentVector = Subtract(end, start);
+            var t = Dot(Subtract(point, start), segmentVector) / Dot(segmentVector, segmentVector);
+            var clampedT = Clamp(t, 0, 1);
+
+            return Lerp(start, end, clampedT);
+        }
+
+        //Implementation of a solution found here
+        //https://math.stackexchange.com/questions/1521128/given-a-line-and-a-point-in-3d-how-to-find-the-closest-point-on-the-line
+        //My testing implies it failes for anything out of bounds. So if the goal was to find the closest point on the infinit line that crosses
+        //between start/end then this would work. I think it could be updated to work however by clamping or checking t. By leaving in unbounded it
+        //finds a different answer when the closest is outside the segment.
+        //
+        //It also does its math a bit differently by defining the segment vector as end - start instead and then using subtraction to determine the final
+        //point instead of addition. Minor changes but threw me off at first.
+        public static Vec3 GetNearestPointOnSegment3(Vec3 point, Vec3 start, Vec3 end)
+        {
+            var t = Dot(Subtract(end, start), Subtract(start, point)) / Dot(Subtract(end, start), Subtract(end, start));
+            var closestPoint = Subtract(start, ScalarMultiplication(Subtract(end, start), t));
+            return closestPoint;
+        }
+
+        //Implementatin of a solution found here
+        //https://diego.assencio.com/?index=ec3d5dfdfc0b6a0d147a656f0af332bd#mjx-eqn-post_ec3d5dfdfc0b6a0d147a656f0af332bd_lambda_closest_point_line_to_point
+        //This was an example for vec2 that I just adapted to work with vec3 by just implementing the vec3 versions of the operations. Appears to work well
+        //but the giant if statements look less nice than implementation 2.
+        public static Vec3 GetNearestPointOnSegment4(Vec3 point, Vec3 start, Vec3 end)
+        {
+            var t = Dot(Subtract(point, start), Subtract(end, start)) / Dot(Subtract(end, start), Subtract(end, start));
+            
+            if(t < 0)
             {
                 return start;
             }
-            else if (lambdaN >= 1)
+            else if(t > 1)
             {
                 return end;
             }
             else
             {
-                return GetN(start, end, lambdaN);
+                return Add(start, ScalarMultiplication(Subtract(end, start), t));
             }
         }
 
-        private static double GetLambdaN(Vec3 point, Vec3 start, Vec3 end)
+        public static Vec3 Add(Vec3 first, Vec3 second)
         {
-            //Stubbed so things compile
-            return 1;
-;        }
-
-        private static Vec3 GetN(Vec3 start, Vec3 end, double lambdaN)
-        {
-            //Stubbed so things compile
-            return new Vec3(1, 2, 3);
+            return new Vec3(first.X + second.X, first.Y + second.Y, first.Z + second.Z);
         }
 
-        private static Vec3 VecSubtract(Vec3 first, Vec3 second)
+        public static Vec3 Subtract(Vec3 first, Vec3 second)
         {
             return new Vec3(first.X - second.X, first.Y - second.Y, first.Z - second.Z);
+        }
+
+        public static Vec3 ScalarMultiplication(Vec3 input, double scalar)
+        {
+            return new Vec3(input.X * scalar, input.Y * scalar, input.Z * scalar);
+        }
+
+        public static double Dot(Vec3 first, Vec3 second)
+        {
+            return (first.X * second.X) + (first.Y * second.Y) + (first.Z * second.Z);
+        }
+
+        public static Vec3 Normalize(Vec3 input)
+        {
+            var magnitude = Magnitude(input);
+            return new Vec3(input.X / magnitude, input.Y / magnitude, input.Z / magnitude);
+        }
+
+        public static double Magnitude(Vec3 input)
+        {
+            return Math.Sqrt(Math.Pow(input.X, 2) + Math.Pow(input.Y, 2) + Math.Pow(input.Z, 2));
+        }
+
+        public static double Clamp(double input, double low, double high)
+        {
+            if (input < low)
+            {
+                return low;
+            }
+            else if( input > high)
+            {
+                return high;
+            }
+            else
+            {
+                return input;
+            }
+        }
+
+        //T must be between 0 - 1 inclusive
+        public static Vec3 Lerp(Vec3 start, Vec3 end, double t)
+        {
+            if (t < 0 || t > 1)
+            {
+                throw new ArgumentException("t must be between 0 and 1 inclusive");
+            }
+
+            return Add(start, ScalarMultiplication(Subtract(end, start), t));
         }
     }
 }
---
+++ SeriousLabsProgrammingTest/Q2/Q2Test/Q2Tests.cs
@@ -0,0 +1,168 @@
+﻿using NUnit.Framework;
+using Q2App;
+
+namespace Q2Test
+{
+    [TestFixture]
+    public class Q2Tests
+    {
+        [Test]
+        public void DotProduct()
+        {
+            var a = new Vec3(1, 2, 3);
+            var b = new Vec3(1, 5, 7);
+
+            var result = Q2.Dot(a, b);
+
+            Assert.AreEqual(32, result);
+        }
+
+        [Test]
+        public void Add()
+        {
+            var a = new Vec3(4, 2, -1);
+            var b = new Vec3(-1, 2, 3);
+
+            var result = Q2.Add(a, b);
+
+            Assert.AreEqual(new Vec3(3, 4, 2), result);
+        }
+
+        [Test]
+        public void Subtract()
+        {
+            var a = new Vec3(4, 2, -1);
+            var b = new Vec3(-1, 2, 3);
+
+            var result = Q2.Subtract(a, b);
+
+            Assert.AreEqual(new Vec3(5, 0, -4), result);
+        }
+
+        [Test]
+        public void Normalize()
+        {
+            var a = new Vec3(3, -4, 0);
+            var result = Q2.Normalize(a);
+
+            Assert.AreEqual(new Vec3(0.6, -0.8, 0), result);
+        }
+
+        [Test]
+        public void Magnitude()
+        {
+            var a = new Vec3(3, -4, 0);
+
+            var result = Q2.Magnitude(a);
+
+            Assert.AreEqual(5, result);
+        }
+
+        [Test]
+        public void ScalarMultiplication()
+        {
+            var a = new Vec3(3, -2, 6);
+
+            var result = Q2.ScalarMultiplication(a, 5);
+
+            Assert.AreEqual(new Vec3(15, -10, 30), result);
+        }
+
+        [Test]
+        public void Clamp()
+        {
+            Assert.AreEqual(2, Q2.Clamp(2, 1, 5));
+            Assert.AreEqual(5, Q2.Clamp(2, 5, 8));
+            Assert.AreEqual(8, Q2.Clamp(10, 1, 8));
+        }
+
+        [Test]
+        public void ImplementationEquivalency()
+        {
+            var a = new Vec3(2, -5, 8);
+            var b = new Vec3(5, 3, 7);
+            var point = new Vec3(2, -1, 3);
+
+            var result = Q2.GetNearestPointOnSegment(point, a, b);
+            var result2 = Q2.GetNearestPointOnSegment2(point, a, b);
+            var result3 = Q2.GetNearestPointOnSegment3(point, a, b);
+            var result4 = Q2.GetNearestPointOnSegment3(point, a, b);
+
+            //Assert.AreEqual(result, result2);
+            Assert.AreEqual(result2, result3);
+            Assert.AreEqual(result3, result4);
+        }
+
+        [Test]
+        public void ImplementationEquivalency2()
+        {
+            var a = new Vec3(1, 2, 0);
+            var b = new Vec3(3, 2, 0);
+            var point = new Vec3(2, 4, 0);
+
+            var result = Q2.GetNearestPointOnSegment(point, a, b);
+            var result2 = Q2.GetNearestPointOnSegment2(point, a, b);
+            var result3 = Q2.GetNearestPointOnSegment3(point, a, b);
+            var result4 = Q2.GetNearestPointOnSegment4(point, a, b);
+
+            Assert.AreEqual(new Vec3(2 ,2, 0), result);
+            Assert.AreEqual(new Vec3(2, 2, 0), result2);
+            Assert.AreEqual(new Vec3(2, 2, 0), result3);
+            Assert.AreEqual(new Vec3(2, 2, 0), result4);
+        }
+
+        [Test]
+        public void ImplementationEquivalency3()
+        {
+            var a = new Vec3(1, 2, 0);
+            var b = new Vec3(3, 2, 0);
+            var point = new Vec3(-1, 4, 0);
+
+            var result = Q2.GetNearestPointOnSegment(point, a, b);
+            var result2 = Q2.GetNearestPointOnSegment2(point, a, b);
+            var result3 = Q2.GetNearestPointOnSegment3(point, a, b);
+            var result4 = Q2.GetNearestPointOnSegment4(point, a, b);
+
+            //Assert.AreEqual(new Vec3(1, 2, 0), result);
+            Assert.AreEqual(new Vec3(1, 2, 0), result2);
+            //Assert.AreEqual(new Vec3(1, 2, 0), result3);
+            Assert.AreEqual(new Vec3(1, 2, 0), result4);
+        }
+
+        [Test]
+        public void ImplementationEquivalency4()
+        {
+            var a = new Vec3(1, 2, 0);
+            var b = new Vec3(3, 2, 0);
+            var point = new Vec3(5, 4, 0);
+
+            var result = Q2.GetNearestPointOnSegment(point, a, b);
+            var result2 = Q2.GetNearestPointOnSegment2(point, a, b);
+            var result3 = Q2.GetNearestPointOnSegment3(point, a, b);
+            var result4 = Q2.GetNearestPointOnSegment4(point, a, b);
+
+            //Assert.AreEqual(new Vec3(3, 2, 0), result);
+            Assert.AreEqual(new Vec3(3, 2, 0), result2);
+            //Assert.AreEqual(new Vec3(3, 2, 0), result3); //Returns the unbounded result
+            Assert.AreEqual(new Vec3(3, 2, 0), result4);
+        }
+
+        [Test]
+        public void ImplementationEquivalency5()
+        {
+            var a = new Vec3(0, 1, 2);
+            var b = new Vec3(0, 3, 2);
+            var point = new Vec3(0, 2, 4);
+
+            var result = Q2.GetNearestPointOnSegment(point, a, b);
+            var result2 = Q2.GetNearestPointOnSegment2(point, a, b);
+            var result3 = Q2.GetNearestPointOnSegment3(point, a, b);
+            var result4 = Q2.GetNearestPointOnSegment4(point, a, b);
+
+            Assert.AreEqual(new Vec3(0, 2, 2), result);
+            Assert.AreEqual(new Vec3(0, 2, 2), result2);
+            Assert.AreEqual(new Vec3(0, 2, 2), result3);
+            Assert.AreEqual(new Vec3(0, 2, 2), result4);
+        }
+    }
+}
